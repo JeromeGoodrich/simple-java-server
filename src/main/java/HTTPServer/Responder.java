3315f7545fe0ba977a3c4e4286fed36ac1de188d
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-02-26 11:39:40 -0600

    add responder tests for responseBody

--- src/main/java/HTTPServer/Responder.java
+++ src/main/java/HTTPServer/Responder.java
@@ -1,17 +1,29 @@
 package HTTPServer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Responder {
 
     private String status;
+    private ArrayList responseBody;
 
     public void setStatusLine(String status) {
         this.status = status;
     }
 
+    public void setResponseBody(ArrayList responseBody) {
+        this.responseBody = responseBody;
+    }
+
     public String getStatusLine() {
         return status;
+    }
+
+    public ArrayList getResponseBody() {
+        return responseBody;
     }
 
     public void createStatusLine(String path, String version) {
@@ -26,4 +38,37 @@ public class Responder {
             }
         }
     }
-}
+
+    public void createResponseBody(String path) {
+        if (path == "/") {
+            ArrayList body = new ArrayList<String>();
+            body.add("Hello World");
+            setResponseBody(body);
+        } else {
+            String fileName = path.replaceFirst("/","");
+            if (new File(fileName).isDirectory()) {
+                File dir = new File(fileName);
+                String[] dirContents = dir.list();
+                ArrayList files = new ArrayList<String>();
+                for (String s: dirContents)
+                    files.add(s);
+                setResponseBody(files);
+            } else {
+                if (new File(fileName).isFile())
+                    try {
+                        FileReader fileReader = new FileReader(fileName);
+                        BufferedReader bufferedReader = new BufferedReader(fileReader);
+                        String line = null;
+                        ArrayList lines = new ArrayList<String>();
+                        while ((line = bufferedReader.readLine()) != null) {
+                            lines.add(line);
+                        }
+                        setResponseBody(lines);
+                    } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                }
+            }
+        }
+    }
+
--- src/test/java/HTTPServer/ResponderTest.java
+++ src/test/java/HTTPServer/ResponderTest.java
@@ -2,6 +2,8 @@ package HTTPServer;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -31,9 +33,38 @@ public class ResponderTest {
     public void testStatusValidPath() {
         Request request = new Request();
         Responder responder = new Responder();
-        request.setPath("/my_files");
+        request.setPath("/src/test/fixtures");
         request.setVersion("1.1");
         responder.createStatusLine(request.getPath(), request.getVersion());
         assertThat(responder.getStatusLine(), is("HTTP/1.1 200 OK\r\n"));
     }
+
+    @Test
+    public void testResponseBodyRoot() {
+        Request request = new Request();
+        Responder responder = new Responder();
+        request.setPath("/");
+        responder.createResponseBody(request.getPath());
+        ArrayList body = new ArrayList<String>();
+        body.add("Hello World");
+        assertThat(responder.getResponseBody(), is(body));
+    }
+
+    @Test
+    public void testResponseBodyFile() {
+        Request request = new Request();
+        Responder responder = new Responder();
+        request.setPath("/src/test/fixtures/my_file.txt");
+        responder.createResponseBody(request.getPath());
+        ArrayList body = new ArrayList<String>();
+        body.add("This is a text file.");
+        body.add("There are many like it, but this one is mine.");
+        assertThat(responder.getResponseBody(), is(body));
+    }
+
 }
+
+//* create map of statuses and reason_phrases,
+//  Other things to test: headers get set up properly
+// body set up properly
+// sends correct data to socket*//
