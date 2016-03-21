7ce7e35a3b2ee2a2b830ea027ce1f64547db1dec
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-03-21 12:29:52 -0500

    refactor Requestbuilder into request, turn HttpParser properties into local variables

--- src/main/java/httpserver/request/Request.java
+++ src/main/java/httpserver/request/Request.java
@@ -1,13 +1,45 @@
 package httpserver.request;
 
 import java.io.InputStream;
+import java.util.Map;
 
-public interface Request {
+public class Request implements Request {
+    private String method;
+    private String path;
+    private String version;
+    private Map<String, String> body;
+    private Map<String, String> headers;
 
-    String getMethod();
-    String getPath();
-    String getVersion();
-    String getHeader(String key);
-    String getBodyVal(String key);
-    InputStream getRawRequest();
+    //constructor is private builder is public builder is defined within class
+    public Request(HTTPRequestBuilder builder) {
+        method = builder.method;
+        path = builder.path;
+        version = builder.version;
+        headers = builder.headers;
+        body = builder.body;
+    }
+
+    public String getMethod() {
+        return this.method;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+
+    public String getHeader(String key) {
+        return this.headers.get(key);
+    }
+
+    public String getBodyVal(String key) {
+        return this.body.get(key);
+    }
+
+    public InputStream getRawRequest() {
+        return null;
+    }
 }
